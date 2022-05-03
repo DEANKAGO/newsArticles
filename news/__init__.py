744061f6d5de671a47aac458388e43c9a61812a1
Dean Kago <martokhago@gmail.com>
2022-05-03 18:20:32 +0300

    created app config

--- news/__init__.py
+++ news/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from news import views
-from .config import DevConfig
+from config import config_options
 
-app = Flask(__name__)
+
+def create_app(config_name):
+  app = Flask(__name__)
+
+
 
 if __name__ == '__main__':
   app.run(debug=True)
