f09ad1fc185e83c8da86cd7105535bd500488a5b
Dean Kago <martokhago@gmail.com>
2022-05-03 22:52:03 +0300

    added list of sources

--- news/views.py
+++ news/views.py
@@ -8,6 +8,8 @@ app = create_app(os.environ.get('FLASK_ENV'))
 NEWS_API_KEY = os.environ.get('API_KEY')
 NEWS_API_BASE_URL = "https://newsapi.org/v2/everything?q=Apple&from=2022-05-03&sortBy=popularity&apiKey=%s" % NEWS_API_KEY
 
+SOURCE_API_BASE_URL = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=%s" % NEWS_API_KEY
+
 
 @app.route('/')
 def index():
@@ -15,7 +17,18 @@ def index():
     list_of_articles = []
     if res.status_code == 200 and res.json()['status'].lower() == "ok":
         list_of_articles = res.json()['articles']
-    return render_template('index.html', list_of_articles=list_of_articles)
+    list_of_sources = []
+    for i in list_of_articles:
+        list_of_sources.append(i['source']['name'])
+    sources_str = ', '.join(list_of_sources)
+    return render_template('index.html', list_of_articles=list_of_articles, sources_str=sources_str)
+
+@app.route('/')
+def bbc():
+    res = requests.get(SOURCE_API_BASE_URL)
+    list_of_sources = []
+    if res.status_code == 200 and res.json()['status'].lower() == "ok":
+        list_of_sources = res.json()['articles']
 
 
 if __name__ == '__main__':
